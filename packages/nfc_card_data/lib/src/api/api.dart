6ba7b894c111509fa21ea19d0901acddd31234f6
Vanessa Berazategui <vberazategui@geocom.com.uy>
2024-03-26 16:09:05 -0300

    chore: regenerate app

--- packages/nfc_card_data/lib/src/api/api.dart
+++ packages/nfc_card_data/lib/src/api/api.dart
@@ -1,6 +1,5 @@
 export 'endpoint.dart';
 export 'http_authorization_interceptor.dart';
-export 'nfc_card_api.dart';
 export 'params/params.dart';
 export 'responses/responses.dart';
 export 'timeout_options.dart';
--- packages/nfc_card_data/lib/src/api/nfc_card_api.dart
+++
@@ -1,7 +0,0 @@
-// Package imports:
-import 'package:retrofit/retrofit.dart';
-
-part 'nfc_card_api.g.dart';
-
-@RestApi()
-abstract class FlutterDddSkeletonMonorepoFlavouredApi {}
